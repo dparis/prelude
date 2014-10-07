510a6c996c0ffa0b020be7c89a5b0b63595bd52c
Dylan Paris <dylan.paris+github@gmail.com>
2014-10-07 11:05:27 -0700

    Update .mc-lists.el

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,6 +3,7 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        electric-graphviz-dot-semi
         prelude-move-beginning-of-line
         sp--self-insert-command
         sp-backward-delete-char
