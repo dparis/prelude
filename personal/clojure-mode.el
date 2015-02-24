a72634e55c227d6c21a22e90ee0c07c8686ba20e
Dylan Paris <dylan.paris+github@gmail.com>
2015-02-24 13:01:24 -0800

    Update clojure mode indentations

--- personal/clojure-mode.el
+++ personal/clojure-mode.el
@@ -2,11 +2,17 @@
 (prelude-require-package 'clojure-mode-extra-font-locking)
 (require 'clojure-mode)
 
+;; Configure CIDER
+; (setq nrepl-hide-special-buffers nil)
+
+;; Enable subword-mode to help dealing with CamelCase identifiers
+(add-hook 'cider-repl-mode-hook #'subword-mode)
+
 ;; Set up custom indents
-(put-clojure-indent 'defcrawler 'defun)
-(put-clojure-indent 'defprocessor 'defun)
-(put-clojure-indent 'facts 'defun)
-(put-clojure-indent 'fact 'defun)
-(put-clojure-indent 'fact-group 'defun)
-(put-clojure-indent 'provided 'defun)
-(put-clojure-indent 'against-background 'defun)
+(put-clojure-indent 'defrecord 1)
+(put-clojure-indent 'expect 1)
+(put-clojure-indent 'expect-let 1)
+(put-clojure-indent 'expect-prop 1)
+(put-clojure-indent 'for-all 1)
+(put-clojure-indent 'side-effects 1)
+(put-clojure-indent 'side-effects-returning 2)
