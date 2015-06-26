e30f0b36503b4b6d3c6b01d240dbadd5184f1d71
Dylan Paris <dylan.paris+github@gmail.com>
2015-06-26 10:38:44 -0700

    Update clojure-mode settings

--- personal/clojure-mode.el
+++ personal/clojure-mode.el
@@ -3,10 +3,16 @@
 (require 'clojure-mode)
 
 ;; Configure CIDER
-; (setq nrepl-hide-special-buffers nil)
+(setq nrepl-hide-special-buffers nil)
+(setq cider-prompt-for-symbol nil)
+(setq cider-repl-use-clojure-font-lock t)
 
 ;; Enable subword-mode to help dealing with CamelCase identifiers
 (add-hook 'cider-repl-mode-hook #'subword-mode)
+
+;; Enable rainbow delimiters mode
+(add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
+
 
 ;; Set up custom indents
 (put-clojure-indent 'defrecord 'defun)
